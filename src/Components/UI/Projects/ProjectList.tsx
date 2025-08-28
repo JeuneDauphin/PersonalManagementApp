16748a677d555081d056b561880cc66adc88ef09
pierre nikolas contessotto <pierrenikolas@gmail.com>
2025-08-28 13:59:22 +0900

    feat: sudo of project cards / list / page and [e]/page

---
+++ src/Components/UI/Projects/ProjectList.tsx
@@ -0,0 +1,9 @@
+//The ProjectList component will display a list of ProjectCard components in a grid layout.
+  //It will receive an array of project objects as props and map through them to render each ProjectCard.
+  //The component will also handle loading states and empty states.
+  //Props:
+  //projects: Array of project objects to be displayed.
+  //isLoading: Boolean to indicate if the data is still loading.
+  //onProjectClick: Function to handle click events on individual project cards.
+  //Example usage:
+  // <ProjectList projects={projects} isLoading={isLoading} onProjectClick={handleProjectClick} />
---
+++ src/Projects/page.tsx
@@ -0,0 +1,4 @@
+//This is the projects page, this page will show all the projects by project card component.
+//The projectCards will be listed as grid in porjectList component.
+//This page will have a search bar and a filter on the top of the page and a button to add new project.
+//This page will also have a pagination at the bottom of the page.
